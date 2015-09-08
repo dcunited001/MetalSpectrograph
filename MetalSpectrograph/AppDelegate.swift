11c74092f4cd8a572f9b3b66efece106b818b06a
David Conner <dconner.pro@gmail.com>
2015-09-07 20:17:37 -0400

    init project

---
+++ MetalSpectrograph/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalSpectrograph
+//
+//  Created by David Conner on 9/7/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalSpectrograph/SpectrographController.swift
@@ -0,0 +1,9 @@
+//
+//  SpectrographController.swift
+//  MetalSpectrograph
+//
+//  Created by David Conner on 9/7/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
