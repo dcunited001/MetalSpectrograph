07a8d868c7bdb6331c978db43eecf3578004c5c2
David Conner <dconner.pro@gmail.com>
2015-09-11 14:14:06 -0400

    Cube: added View and ViewController

--- MetalSpectrograph/CubeView.swift
+++ MetalSpectrograph/CubeView.swift
@@ -10,10 +10,27 @@ import Cocoa
 import MetalKit
 import simd
 
-class CubeViewController: NSViewController {
-    
+class CubeView: MetalView {
+    override func setupRenderPassDescriptor(drawable: CAMetalDrawable) {
+        renderPassDescriptor!.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor!.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor!.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+    }
 }
 
-class CubeView: NSViewController {
+class CubeViewController: NSViewController {
+    var renderer: CubeRenderer!
+    var metalView: CubeView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let rect = CGRect(x: 0, y:0, width: self.view.frame.width, height: self.view.frame.height)
+        renderer = CubeRenderer()
+        metalView = CubeView(frame: rect, device: MTLCreateSystemDefaultDevice())
+        renderer.configure(metalView)
+        metalView.metalViewDelegate = renderer
+        self.view.addSubview(metalView)
+
+    }
 }
