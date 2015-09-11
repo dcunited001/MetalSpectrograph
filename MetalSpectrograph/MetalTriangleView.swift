d677da69cd2152923fccb56a9232795b1f0a8762
David Conner <dconner.pro@gmail.com>
2015-09-11 05:44:12 -0400

    Added BasicTriangleView and MetalTrianglController

--- MetalSpectrograph/MetalTriangleView.swift
+++ MetalSpectrograph/MetalTriangleView.swift
@@ -42,3 +42,49 @@ class MetalTriangleView: MetalView {
         }
     }
 }
+
+class BasicTriangleView: MetalView {
+    override func render() {
+        // setup CFAbsoluteTimeGetCurrent()
+        
+        renderPassDescriptor = currentRenderPassDescriptor
+        
+        // test renderpassdescriptor
+        let commandBuffer = commandQueue.commandBuffer()
+        
+        guard let drawable = currentDrawable else
+        {
+            print("currentDrawable returned nil")
+            
+            return
+        }
+        
+        setupRenderPassDescriptor(drawable)
+        self.metalViewDelegate?.renderObjects(drawable, renderPassDescriptor: renderPassDescriptor!, commandBuffer: commandBuffer)
+        
+        self.metalViewDelegate?.afterRender?()
+    }
+    
+    override func setupRenderPassDescriptor(drawable: CAMetalDrawable) {
+        renderPassDescriptor!.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor!.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor!.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+    }
+}
+
+class MetalTriangleController: NSViewController {
+    var renderer: BasicTriangleRenderer!
+    var metalView: BasicTriangleView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let rect = CGRect(x: 0, y:0, width: self.view.frame.width, height: self.view.frame.height)
+        renderer = BasicTriangleRenderer()
+        metalView = BasicTriangleView(frame: rect, device: MTLCreateSystemDefaultDevice())
+        renderer.configure(metalView)
+        metalView.metalViewDelegate = renderer
+        self.view.addSubview(metalView)
+    }
+}
+
