b5d193fb2c4abc2c894368e1bd0ca54a4f4458a2
David Conner <dconner.pro@gmail.com>
2015-09-13 06:45:30 -0400

    added bufferprovider

---
+++ MetalSpectrograph/BufferProvider.swift
@@ -0,0 +1,15 @@
+//
+//  BufferProvider.swift
+//  MetalSpectrograph
+//
+//  Created by David Conner on 9/13/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+// TODO: implement a bufferprovider
+//
+//class BufferProvider: NSObject {
+//    let inflightBuffers
+//    let kInFlightCommandBuffers = 3
+//
+//    
+//}
