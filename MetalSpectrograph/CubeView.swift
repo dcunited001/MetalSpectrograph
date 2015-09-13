9214fc1410d78977f31561527203a63547964de0
David Conner <dconner.pro@gmail.com>
2015-09-13 06:45:57 -0400

    added a pan gesture to update model rotation

--- MetalSpectrograph/CubeView.swift
+++ MetalSpectrograph/CubeView.swift
@@ -18,6 +18,12 @@ class CubeView: MetalView {
     }
 }
 
+//TODO: Panable
+protocol Panable {
+    var panSensivity:Float { get set } // = 5.0
+    var lastPanLocation: CGPoint! { get set }
+}
+
 class CubeViewController: NSViewController {
     var renderer: CubeRenderer!
     var metalView: CubeView!
@@ -32,10 +38,37 @@ class CubeViewController: NSViewController {
         positionObject()
         metalView.metalViewDelegate = renderer
         self.view.addSubview(metalView)
+        setupGestures()
     }
     
     func positionObject() {
         renderer.object?.modelPosition = float4(0.0, 0.0, 2.0, 1.0)
         renderer.object?.modelScale = float4(0.7, 0.7, 0.7, 1.0)
+    }
+    
+    let panSensivity:Float = 5.0
+    var lastPanLocation: CGPoint!
+    
+    func setupGestures(){
+        var pan = NSPanGestureRecognizer(target: self, action: Selector("pan:"))
+        self.view.addGestureRecognizer(pan)
+    }
+    
+    func pan(panGesture: NSPanGestureRecognizer){
+        if panGesture.state == NSGestureRecognizerState.Changed{
+            var pointInView = panGesture.locationInView(self.view)
+
+            var xDelta = Float((lastPanLocation.x - pointInView.x)/self.view.bounds.width) * panSensivity
+            var yDelta = Float((lastPanLocation.y - pointInView.y)/self.view.bounds.height) * panSensivity
+            
+            
+//            renderer.projectionEye += [yDelta, xDelta, 0.0]
+            renderer.modelRotation += [0.0, 0.0, yDelta, 30*xDelta]
+//            renderer.object?.modelPosition += [yDelta, xDelta, 0.0, 0.0]
+//            renderer.object?.modelRotation += [yDelta, xDelta, 0.0, 0.0]
+            lastPanLocation = pointInView
+        } else if panGesture.state == NSGestureRecognizerState.Began{
+            lastPanLocation = panGesture.locationInView(self.view)
+        } 
     }
 }
