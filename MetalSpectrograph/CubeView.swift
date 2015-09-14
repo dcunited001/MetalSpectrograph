656eac554441b8102c33d9a893b2d994d09b487f
David Conner <dconner.pro@gmail.com>
2015-09-14 13:23:52 -0400

    added sliders to test settings, but it was kinda lame

--- MetalSpectrograph/CubeView.swift
+++ MetalSpectrograph/CubeView.swift
@@ -28,6 +28,19 @@ class CubeViewController: NSViewController {
     var renderer: CubeRenderer!
     var metalView: CubeView!
     
+    @IBOutlet weak var slideScaleX: NSSlider!
+    @IBOutlet weak var slideScaleY: NSSlider!
+    @IBOutlet weak var slideScaleZ: NSSlider!
+    
+    @IBOutlet weak var slideTransformX: NSSlider!
+    @IBOutlet weak var slideTransformY: NSSlider!
+    @IBOutlet weak var slideTransformZ: NSSlider!
+    
+    @IBOutlet weak var slideRotateX: NSSlider!
+    @IBOutlet weak var slideRotateY: NSSlider!
+    @IBOutlet weak var slideRotateZ: NSSlider!
+    @IBOutlet weak var slideRotateTheta: NSSlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,9 +51,45 @@ class CubeViewController: NSViewController {
         renderer.perspectiveAspect = Float(rect.width / rect.height)
         renderer.modelScale = float4(1.0, Float(rect.width / rect.height), 1.0, 1.0)
         positionObject()
+//        initSliders()
         metalView.metalViewDelegate = renderer
         self.view.addSubview(metalView)
         setupGestures()
+    }
+    
+    func initSliders() {
+        slideScaleX.minValue = 0.0
+        slideScaleX.maxValue = 5.0
+        slideScaleY.minValue = 0.0
+        slideScaleY.maxValue = 5.0
+        slideScaleZ.minValue = 0.0
+        slideScaleZ.maxValue = 5.0
+        slideScaleX.floatValue = renderer.object!.modelScale.x
+        slideScaleY.floatValue = renderer.object!.modelScale.y
+        slideScaleZ.floatValue = renderer.object!.modelScale.z
+        
+        slideTransformX.minValue = -10.0
+        slideTransformX.maxValue = 10.0
+        slideTransformY.minValue = -10.0
+        slideTransformY.maxValue = 10.0
+        slideTransformZ.minValue = -10.0
+        slideTransformZ.maxValue = 10.0
+        slideTransformX.floatValue = renderer.object!.modelPosition.x
+        slideTransformY.floatValue = renderer.object!.modelPosition.y
+        slideTransformZ.floatValue = renderer.object!.modelPosition.z
+        
+        slideRotateX.minValue = -1.0
+        slideRotateX.maxValue = 1.0
+        slideRotateY.minValue = -1.0
+        slideRotateY.maxValue = 1.0
+        slideRotateZ.minValue = -1.0
+        slideRotateZ.maxValue = 1.0
+        slideRotateTheta.minValue = -540.0
+        slideRotateTheta.maxValue = 540.0
+        slideRotateX.floatValue = renderer.object!.modelRotation.x
+        slideRotateY.floatValue = renderer.object!.modelRotation.y
+        slideRotateZ.floatValue = renderer.object!.modelRotation.z
+        slideRotateTheta.floatValue = renderer.object!.modelRotation.w
     }
     
     func positionObject() {
@@ -52,14 +101,22 @@ class CubeViewController: NSViewController {
     var lastPanLocation: CGPoint!
     
     func setupGestures(){
+//        var pan = NSPanGestureRecognizer(target: self, action: Selector("updateObjectToSliderValues:"))
         var pan = NSPanGestureRecognizer(target: self, action: Selector("pan:"))
-        self.view.addGestureRecognizer(pan)
+        metalView.addGestureRecognizer(pan)
+    }
+
+    func updateObjectToSliderValues(panGesture: NSPanGestureRecognizer) {
+        renderer.object?.modelPosition = [slideTransformX.floatValue, slideTransformY.floatValue, slideTransformZ.floatValue, 1.0]
+        print(renderer.object?.modelScale)
+        renderer.object?.modelScale = [slideScaleX.floatValue, slideScaleY.floatValue, slideScaleZ.floatValue, 1.0]
+        renderer.object?.modelRotation = [slideRotateX.floatValue, slideRotateY.floatValue, slideRotateZ.floatValue, slideRotateTheta.floatValue]
     }
     
     func pan(panGesture: NSPanGestureRecognizer){
         if panGesture.state == NSGestureRecognizerState.Changed{
             var pointInView = panGesture.locationInView(self.view)
-
+            
             var xDelta = Float((lastPanLocation.x - pointInView.x)/self.view.bounds.width) * panSensivity
             var yDelta = Float((lastPanLocation.y - pointInView.y)/self.view.bounds.height) * panSensivity
             
