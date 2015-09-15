7f635da7e8d7c7b4bf77b542268c8caf0b2d5f32
David Conner <dconner.pro@gmail.com>
2015-09-15 06:50:23 -0400

    Added gestures to TexturedQuadViewController

--- MetalSpectrograph/TexturedQuadView.swift
+++ MetalSpectrograph/TexturedQuadView.swift
@@ -15,6 +15,10 @@ import simd
 //willEnterForeground
 //didEnterBackground
 
+class TexturedQuadView: MetalView {
+    
+}
+
 class TexturedQuadViewController: NSViewController {
     var renderer: TexturedQuadImgRenderer!
     var metalView: TexturedQuadView!
@@ -26,19 +30,52 @@ class TexturedQuadViewController: NSViewController {
         renderer = TexturedQuadImgRenderer()
         metalView = TexturedQuadView(frame: rect, device: MTLCreateSystemDefaultDevice())
         renderer.configure(metalView)
-
+        renderer.perspectiveAspect = Float(rect.width / rect.height)
+        positionTexture()
         metalView.metalViewDelegate = renderer
 
         self.view.addSubview(metalView)
+        setupGestures()
     }
     
     func positionTexture() {
-        renderer.object!.modelRotation = float4(1.0, 0.0, 0.0, 0.0)
+        renderer.object!.modelRotation = float4(1.0, 0.0, 0.0, 90.0)
         renderer.object!.modelPosition = float4(0.0, 0.0, 10.0, 1.0)
     }
     
-}
-
-class TexturedQuadView: MetalView {
+    let panSensivity:Float = 5.0
+    var lastPanLocation: CGPoint!
     
+    func setupGestures(){
+        //        var pan = NSPanGestureRecognizer(target: self, action: Selector("updateObjectToSliderValues:"))
+        var pan = NSPanGestureRecognizer(target: self, action: Selector("pan:"))
+        metalView.addGestureRecognizer(pan)
+    }
+    
+    func pan(panGesture: NSPanGestureRecognizer){
+        if panGesture.state == NSGestureRecognizerState.Changed{
+            var pointInView = panGesture.locationInView(self.view)
+            
+            var xDelta = Float((lastPanLocation.x - pointInView.x)/self.view.bounds.width) * panSensivity
+            var yDelta = Float((lastPanLocation.y - pointInView.y)/self.view.bounds.height) * panSensivity
+            
+            //            renderer.perspectiveAngle += xDelta
+            //            renderer.perspectiveNear += yDelta/10
+            //            renderer.perspectiveFar += xDelta
+            //            renderer.projectionEye += [0.0, 0.0, yDelta]
+            //            renderer.modelRotation += [0.0, 0.0, yDelta, 30*xDelta]
+            //            renderer.object?.modelPosition += [0.0, 0.0, yDelta, 0.0]
+            //            renderer.object?.modelPosition += [0.0, 0.0, 0.0, yDelta]
+            //            renderer.object?.modelRotation += [0.0, 0.0, yDelta, 30*xDelta]
+                        renderer.object?.modelRotation += [0.0, 0.0, yDelta, 60*xDelta]
+//            renderer.object?.modelRotation += [0.0, 0.0, 0.0, 60*xDelta]
+//              renderer.object?.modelPosition += [xDelta, 0.0, yDelta, 0.0]
+//            renderer.object?.modelPosition += [0.0, 0.0, yDelta, 0.0]
+            //                        renderer.object?.modelScale += [xDelta, yDelta, 0.0, 0.0]
+            
+            lastPanLocation = pointInView
+        } else if panGesture.state == NSGestureRecognizerState.Began{
+            lastPanLocation = panGesture.locationInView(self.view)
+        }
+    }
 }
