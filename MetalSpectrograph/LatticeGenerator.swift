8370a4d20a616eb555e9af2ab6e7d10b0df78a58
David Conner <dconner.pro@gmail.com>
2015-09-18 09:17:07 -0400

    continuing to brainstorm lattice stuff

--- MetalSpectrograph/LatticeGenerator.swift
+++ MetalSpectrograph/LatticeGenerator.swift
@@ -9,12 +9,21 @@
 import simd
 import Metal
 
-// tensors for manipulating lattice
+// TODO: tensors for manipulating lattice
 // - tensor representing & updating the immediate state
 // - 'differentiated' tensor representing the state change across time
+// - each vertex has n adjacent nodes, even edge nodes, where n also == the number of params for tensor
+//   - for now, treat as infinite lattice for simplicity
+//   - thus, each vertex can be assigned a unique id in a 1D array
+//   - but also, each vertex's specific tensor attribute can be addressed with (vertexId, attrId)
+// - define a method for passing tensor information to adjacent vertices
+//   - this will likely be in the vertex or fragment shader
 
 protocol Lattice {
     var lattice: Modelable? { get set }
+//    var adjacentNodeCount: { get set }
+    var tensorBuffer: MTLBuffer? { get set }
+    var tensorBufferId: Int { get set }
     
     // define indexing for vertices
     func getVertex(id: Int) -> Vertexable
@@ -23,13 +32,14 @@ protocol Lattice {
     func generateLattice(node: Modelable)
     
     // define tensors for updating the lattice
-    
-    
+
     // write to buffer
 }
 
-class Lattice2D<N: protocol<Vertexable, Chunkable>>: Node<N>, Lattice {
+class Lattice2D<N: protocol<Vertexable, Chunkable>>: Node<N>, Lattice, RenderEncodable {
     var lattice: Modelable?
+    var tensorBuffer: MTLBuffer?
+    var tensorBufferId: Int = 1
     
     func getVertex(id: Int) -> Vertexable {
         return N(chunks: [float4(1.0, 1.0, 1.0, 1.0)])
@@ -39,6 +49,11 @@ class Lattice2D<N: protocol<Vertexable, Chunkable>>: Node<N>, Lattice {
         // assume 6 points input, if a base node
         // read in each triangle
         // convert it into 4 triangles
+    }
+    
+    func encode(renderEncoder: MTLRenderCommandEncoder) {
+        //write vertexbuffer
+        //write tensorbuffer
     }
 }
 
