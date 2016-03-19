9f5a690b40299c7d44fb8b598aae8b2ecab65004
David Conner <dconner.pro@gmail.com>
2016-03-19 01:13:11 -0400

    a few fixes

--- MetalSpectrograph/ParticleLabView.swift
+++ MetalSpectrograph/ParticleLabView.swift
@@ -181,7 +181,7 @@ class ParticleLabView: MetalView, MetalViewDelegate {
     func afterRender() {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
             {
-                particleLabDelegate?.particleLabDidUpdate()
+                self.particleLabDelegate?.particleLabDidUpdate()
         }
     }
     
